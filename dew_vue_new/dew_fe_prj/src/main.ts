9bb9846ac33491b574dec9c6f97ef103d453c2a7
dew <dew.lee@kakaobank.com>
2023-06-13 13:34:16 +0900

    개발 : 최초 등록

---
+++ dew_vue_new/dew_fe_prj/src/main.ts
@@ -0,0 +1,16 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import HelloWorld from './components/HelloWorld.vue'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+app.component(HelloWorld)
+
+app.mount('#app')
